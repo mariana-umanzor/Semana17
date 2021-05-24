e87a4151071ccc734b012b726bf1de6ecda93158
Mariana <mariana.umanzor.ugb@gmail.com>
2021-05-23 18:07:04 -0600

    Creamos el primer modulo mas la clase base

---
+++ index.js
@@ -0,0 +1,13 @@
+class Mariana {
+    constructor(nombre, materia, tarea) {
+      this.nombre = nombre;
+      this.materia = materia;
+      this.tarea = tarea;
+    }
+  }
+
+  const Mariana_clases = [
+    new Mariana('Mariana', 'programacion', 'Exportar modulos'),
+    new Mariana('Mariana', 'base datos', 'sql server'),
+    new Mariana('Mariana', 'ingles', 'The Verbs'),
+  ];
