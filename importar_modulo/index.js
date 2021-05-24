22ae0a2a68412dfe8c4238e6ea8335e8f911cbf8
Mariana <mariana.umanzor.ugb@gmail.com>
2021-05-23 18:14:44 -0600

    Creamos un nuevo modulo que importa el primero y lo accede

---
+++ importar_modulo/index.js
@@ -0,0 +1,4 @@
+const colors = require('Semana17');
+
+const materia = colors.get_clase_aleatorea();
+console.log(`Soy:  ${materia.nombre} y curso la materia de  ${materia.materia}  con el tema de: ${materia.tarea}`);
